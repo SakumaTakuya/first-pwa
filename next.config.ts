83cd3bb93ae368bde308f3adc0408ebe89d7bb93
SakumaTakuya <mo.takusakkun@gmail.com>
2025-08-16 14:44:43 +0900

    github pages 設定

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 import withPWA from "next-pwa";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/first-pwa',
   /* config options here */
 };
 
